db0dfdb1c5b8000a30fab2db38c1e6fee394bdfe
Jose Rubilar <jose.rubilar0@walmart.com>
2020-11-29 01:56:52 -0300

    commit de cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,27 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
-  {
+/*  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'developers',
+    loadChildren: () => import('./pages/developers/developers.module').then( m => m.DevelopersPageModule)
+  },
+  {
+    path: 'developer',
+    loadChildren: () => import('./pages/developer/developer.module').then( m => m.DeveloperPageModule)
+  },*/
+
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'developers', loadChildren: './pages/developers/developers.module#DevelopersPageModule' },
+  { path: 'developers/:id', loadChildren: './pages/developer/developer.module#DeveloperPageModule' },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  }
 ];
 
 @NgModule({
---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
+import { NgForm} from '@angular/forms';
+import { DatabaseService, User, Dev } from './../../services/database.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  users: User[] = [];
+  public theTodo: any;
+
+  selectedView = 'devs';
+  errorMessage = ''
+ 
+  constructor(private db: DatabaseService, private navCtrl: NavController) {
+   }
+
+  ngOnInit() {
+  }
+
+  login(form: NgForm) {
+    const { username, password } = form.value;
+    this.db.getUser(username,password).then(data => {
+
+      if(data){
+        this.navCtrl.navigateRoot('/developers');
+      }else{
+        this.theTodo = 'Something went wrong!' ;
+      }
+  }).catch( err => {
+    this.errorMessage = 'usuario o clave incorrectas'
+  }) 
+  }
+
+}
